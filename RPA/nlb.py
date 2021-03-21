fd146429d2814217fd37becf0293cc6bed860f46
AftTheRain <onnweicheng@gmail.com>
2021-03-21 12:21:29 +0800

    Return True if there are search results, False otherwise

--- RPA/nlb.py
+++ RPA/nlb.py
@@ -13,36 +13,42 @@ def nlb_search(search_query):
 
         titles_on_page = t.count('(//div[@class="CoverImageContainer"])')
         print(f'Titles on page: {titles_on_page}')
+        print(f'---------------------------')
 
-        for i in range(1, titles_on_page + 1):
-            t.hover(f'(//div[@class="CoverImageContainer"])[{i}]')
-            search_results = t.read(f'(//div[@class="CoverImageContainer"])[{i}]/a/@href')
-            t.url(f'{url}{search_results}')
-            print(f'Going to: {url}{search_results}')
+        if titles_on_page > 0:
+            for i in range(1, titles_on_page + 1):
+                t.hover(f'(//div[@class="CoverImageContainer"])[{i}]')
+                search_results = t.read(f'(//div[@class="CoverImageContainer"])[{i}]/a/@href')
+                t.url(f'{url}{search_results}')
+                print(f'Going to: {url}{search_results}')
 
-            title = t.read('//h1')
-            sub_title = t.read('//h1/following-sibling::div[@class="TitleSeries"]')
-            author = t.read('//h1/following-sibling::div[@class="TitleDetailsHeading-creator"]')
-            book_type = t.read('//h1/following-sibling::span')
-            ratings = t.read('//h1/../following-sibling::div[@class="js-starRatingsContainer"]//@data-global-rating')
-            availablity = t.read('//h1/../following-sibling::div[@class="show-for-600-up js-copiesAvailableContainer"]//span')
-            
-            print(f'Title      : {title}')
-            print(f'Subtitle   : {sub_title}')
-            print(f'Author     : {author}')
-            print(f'Book Type  : {book_type}')
-            print(f'Ratings    : {ratings}')
-            print(f'Availablity: {availablity}')
-            print(f'---------------------------')
+                title = t.read('//h1')
+                sub_title = t.read('//h1/following-sibling::div[@class="TitleSeries"]')
+                author = t.read('//h1/following-sibling::div[@class="TitleDetailsHeading-creator"]')
+                book_type = t.read('//h1/following-sibling::span')
+                ratings = t.read('//h1/../following-sibling::div[@class="js-starRatingsContainer"]//@data-global-rating')
+                availablity = t.read('//h1/../following-sibling::div[@class="show-for-600-up js-copiesAvailableContainer"]//span')
+                
+                print(f'Title      : {title}')
+                print(f'Subtitle   : {sub_title}')
+                print(f'Author     : {author}')
+                print(f'Book Type  : {book_type}')
+                print(f'Ratings    : {ratings}')
+                print(f'Availablity: {availablity}')
+                print(f'---------------------------')
 
-            t.url(f'{url}{search_query_prefix}{search_query}')
+                t.url(f'{url}{search_query_prefix}{search_query}')
 
-        t.close()
+            t.close()
+            return True
+
+        else:
+            return False
 
     finally:
         t.close()
 
 #main
 import sys
-nlb_search(sys.argv[1])
+print(nlb_search(sys.argv[1]))
 
