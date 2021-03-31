af3a5705fce5ff61e28ae9ffc6a1e5b58a65c1df
JS-code-dev <60381845+JS-code-dev@users.noreply.github.com>
2021-04-01 00:24:28 +0800

    Update app.py

--- DF/app.py
+++ DF/app.py
@@ -1,9 +1,8 @@
 from flask import Flask, request, make_response, jsonify
 import requests
 import json
-import pandas as pd
-import os
 import time
+import threading
 
 app = Flask(__name__)
 bookList = []
@@ -13,6 +12,24 @@ bookListLimit = 3
 # UTIL FUNCTIONS : START
 # **********************
 
+def startRPA(books):
+    print("Starting RPA...")
+    time.sleep(300)
+    print(books)
+    print("End of RPA...")
+
+def clearContext(context):
+    clearedContext = []
+    for cntxt in context:
+        cntxt["lifespanCount"] = 0
+        clearedContext.append(cntxt)
+    return clearedContext
+
+def clearBookList():
+    global bookList
+    bookList = []
+
+x = threading.Thread(target = startRPA, args = (bookList, ))
 
 # **********************
 # UTIL FUNCTIONS : END
@@ -22,6 +39,41 @@ bookListLimit = 3
 # Intent Handlers funcs : START
 # *****************************
 
+def welcomeIntentHandler(context):
+    global x
+    resp = ""
+    if x.is_alive():
+        clearContext(context)
+        resp = "Hello! I am your friendly neighbourhood Librarian. I am still working on your previous enquiry. Please check back in again after a few minutes!"
+    else:
+        resp = "Hello! I am your friendly neighbourhood Librarian. What book would you like to borrow from NLB?"
+    return(resp, context)
+
+def bookTitleIntentHandler(title, context):
+    global x
+    bookList.append(title)
+    resp = ""
+    if len(bookList) == bookListLimit:
+        x = threading.Thread(target = startRPA, args = (bookList, ))
+        x.start()
+        context = clearContext(context)
+        clearBookList()
+        resp = "Ok! I have a full list of the books that you have enquired. I will send you an email on the ratings and availability. Thank you and Goodbye!"
+    else:
+        resp = "Would you like to enquire about another book? Current book list:"
+        for book in bookList:
+            resp = resp + " " + book + ","
+        resp = resp[:-1]
+    return (resp, context)
+
+def bookTitleIntentEndHandler(context):
+    global x
+    x = threading.Thread(target = startRPA, args = (bookList, ))
+    x.start()
+    context = clearContext(context)
+    clearBookList()
+    resp = "Ok! I will send you an email on the ratings and availability of the books that you have enquired. Thank you and Goodbye!"
+    return (resp, context)
 
 # ***************************
 # Intent Handlers funcs : END
@@ -36,38 +88,35 @@ def webhook():
 
     req = request.get_json(silent=True, force=True)
     print(req)
+
     intent_name = req["queryResult"]["intent"]["displayName"]
     session = req["session"]
     currentContext = req["queryResult"]["outputContexts"]
 
     resp_text = "Unable to find a matching intent. Try again."
-    if intent_name == "GetBookTitleIntent":
+
+    if intent_name == "Welcome Intent":
+        response = welcomeIntentHandler(currentContext)
+        resp_text = response[0]
+        respContext = response[1]
+
+    elif intent_name == "GetBookTitleIntent":
         bookTitle = req["queryResult"]["parameters"]["book-title"]
-        bookList.append(bookTitle)
+        response = bookTitleIntentHandler(bookTitle, currentContext)
+        resp_text = response[0]
+        respContext = response[1]
 
-        if len(bookList) == bookListLimit:
-        # Parse book title through to NLB/Amazon interface. Response will be set as default due to time out
-            time.sleep(5)
-
-        resp_text = "Would you like to enquire about another book?  \nCurrent book list:"
-        for book in bookList:
-            resp_text = resp_text + " " + book + ","
-        resp_text = resp_text[:-1]
     elif intent_name == "GetBookTitleIntent - no":
-        # Parse book title through to NLB/Amazon interface.Response will be set as default due to time out
-        time.sleep(5)
+        response = bookTitleIntentEndHandler(currentContext)
+        resp_text = response[0]
+        respContext = response[1]
+
     else:
         resp_text = "Unable to find a matching intent. Try again."
 
-    # Clearing context and resetting the chat bot
-    # respContext = []
-    # for context in currentContext:
-    #     context["lifespanCount"] = 0
-    #     respContext.append(context)
-
     return make_response(jsonify({
         'fulfillmentText': resp_text,
-        # 'outputContexts': respContext
+        'outputContexts': respContext
         })
         )
 
