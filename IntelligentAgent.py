d6f53835e9109e6837f1aaa2f9f92a22d36da17a
JS-code-dev <60381845+JS-code-dev@users.noreply.github.com>
2021-04-13 22:00:19 +0800

    Create IntelligentAgent.py

---
+++ IntelligentAgent.py
@@ -0,0 +1,15 @@
+import ...
+
+class IntelligentAgent:
+  def __init__(self):
+    Load model
+
+  def compareTitleFn (title, list of compring title):
+    ..
+    ..
+    return nearest title
+
+
+  def punchline(abstract as string):
+    return punchline as string
+    
