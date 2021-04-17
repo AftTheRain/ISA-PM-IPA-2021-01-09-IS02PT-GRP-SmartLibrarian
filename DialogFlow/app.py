608a72ddfa489f5e986d9e198d49f7ead3821cb9
JS-code-dev <60381845+JS-code-dev@users.noreply.github.com>
2021-04-17 22:08:25 +0800

    Rename dialog flow

--- DialogFlow/app.py
+++ DialogFlow/app.py
@@ -8,8 +8,11 @@ import sys
 
 dir_main = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(os.path.join(dir_main, "RPA"))
+dir_main = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.join(dir_main, "IntelligentBookAnalytics"))
 
 import RPA
+import IntelligentBookAnalytics as IBA
 
 app = Flask(__name__)
 bookList = []
